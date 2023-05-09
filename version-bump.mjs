b80e8b1f8db3d55f9e76c6f827beeea5081f604f
A-F-V <faracea2000@gmail.com>
2023-05-09 23:52:25 +0100

    Bump version to 1.0.1

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,19 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from 'fs';
 
 const targetVersion = process.env.npm_package_version;
 
 // read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
 versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+
+// Create a git tag for the new version
+import { execSync } from 'child_process';
+execSync(`git add . && git commit -m "Bump version to ${targetVersion}"`);
+execSync(`git tag -a v${targetVersion} -m "v${targetVersion}"`);
